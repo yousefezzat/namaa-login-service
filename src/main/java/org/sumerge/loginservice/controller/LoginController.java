b998f68e5d5e1aca3bd7b0b3acbdbbaed09879cd
Yousef <yousefezzat442@gmail.com>
2024-02-12 17:24:05 +0200

    added logger for login request and make national id to be passed as request parameter

--- src/main/java/org/sumerge/loginservice/controller/LoginController.java
+++ src/main/java/org/sumerge/loginservice/controller/LoginController.java
@@ -1,30 +1,32 @@
 package org.sumerge.loginservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.sumerge.loginservice.model.nafaz_response.NafazResponse;
 import org.sumerge.loginservice.service.LoginService;
-
-import java.util.logging.Logger;
 
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
     private final LoginService loginService;
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @PostMapping("/login")
-    public ResponseEntity<NafazResponse> login(@RequestBody String nationalId) {
+    public ResponseEntity<NafazResponse> login(@RequestBody String nationalId, HttpServletRequest request) {
+        logger.info("Received {} request to {} with headers: {}", request.getMethod(), request.getRequestURI(), request.getHeaderNames());
+        logger.info("Received login request for nationalId: {}", nationalId);
         NafazResponse nafazResponse = loginService.login(nationalId);
-
-            return  ResponseEntity.ok(nafazResponse);
-
+        return ResponseEntity.ok(nafazResponse);
     }
-
     @PostMapping("/callback")
     public ResponseEntity<Void> callback() {
         return ResponseEntity.ok().build(); //temporary
     }
-
 }
